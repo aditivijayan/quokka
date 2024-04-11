fffe216d3c6a68353f7ddc0a8def64e8c46e3c62
aditivijayan <vijayanaditi@gmail.com>
2024-04-11 02:06:13 -0400

    comment out lines adding phigas and those setting SN density to 0

--- src/MetalAdvectionProblem/test_sne.cpp
+++ src/MetalAdvectionProblem/test_sne.cpp
@@ -134,7 +134,7 @@ void RadhydroSimulation<NewProblem>::setInitialConditionsOnGrid(quokka::grid gri
       
       double Phigas = 2.* 3.1415 * Const_G * Sigma_gas * std::abs(z);
 
-      double Phitot = Phist + Phidm + Phigas;
+      double Phitot = Phist + Phidm; // + Phigas;
 
 			double rho, rho_disk, rho_halo;
              rho_disk = rho01 * std::exp(-Phitot/std::pow(sigma1,2.0)) ;
@@ -275,7 +275,7 @@ void AddSupernova(amrex::MultiFab &mf, amrex::GpuArray<Real, AMREX_SPACEDIM> pro
         if(x0<0.5*dx[0] && y0<0.5*dx[1] && z0< 0.5*dx[2] ) {
         state(i, j, k, HydroSystem<NewProblem>::energy_index)         +=   rho_eint_blast; 
         state(i, j, k, HydroSystem<NewProblem>::internalEnergy_index) +=    rho_eint_blast; 
-       //  state(i, j, k, HydroSystem<NewProblem>::density_index) = 1.e-2 * Const_mH;
+        //state(i, j, k, HydroSystem<NewProblem>::density_index) = 1.e-2 * Const_mH;
         state(i, j, k, Physics_Indices<NewProblem>::pscalarFirstIndex+1)+=  1.e3/cell_vol;
         // printf("The location of SN=%d,%d,%d\n",i, j, k);
         // printf("SN added at level=%d\n", level);
@@ -364,7 +364,7 @@ HydroSystem<NewProblem>::GetGradFixedPotential(amrex::GpuArray<amrex::Real, AMRE
        double z      = posvec[2];
        grad_potential[2]  = 2.* 3.1415 * Const_G * rho_dm * std::pow(R0,2) * (2.* z/std::pow(R0,2))/(1. + std::pow(z,2)/std::pow(R0,2));
        grad_potential[2] += 2.* 3.1415 * Const_G * Sigma_star * (z/z_star) * (std::pow(1. + z*z/(z_star*z_star), -0.5));
-       grad_potential[2] += 2.* 3.1415 * Const_G * Sigma_gas * std::abs(z)/z; //gas potential
+   //    grad_potential[2] += 2.* 3.1415 * Const_G * Sigma_gas * std::abs(z)/z; //gas potential
     #endif
 
 return grad_potential;
@@ -464,7 +464,7 @@ AMREX_GPU_DEVICE AMREX_FORCE_INLINE void AMRSimulation<NewProblem>::setCustomBou
    }
 
     const double rho_edge   = consVar(i, j, kedge, HydroSystem<NewProblem>::density_index);
-		const double x1Mom_edge = consVar(i, j, kedge, HydroSystem<NewProblem>::x1Momentum_index);
+    const double x1Mom_edge = consVar(i, j, kedge, HydroSystem<NewProblem>::x1Momentum_index);
     const double x2Mom_edge = consVar(i, j, kedge, HydroSystem<NewProblem>::x2Momentum_index);
           double x3Mom_edge = consVar(i, j, kedge, HydroSystem<NewProblem>::x3Momentum_index);
     const double etot_edge  = consVar(i, j, kedge, HydroSystem<NewProblem>::energy_index);
