fb63ed2eb6e3a656605f5d50993361877ac38af6
Aditi Vijayan <aditi@setonix-01.pawsey.org.au>
2024-11-06 11:28:57 +0800

    add units

--- src/problems/MetalAdvectionProblem/test_sne.cpp
+++ src/problems/MetalAdvectionProblem/test_sne.cpp
@@ -117,6 +117,7 @@ template <> struct Physics_Traits<NewProblem> {
 	static constexpr int numMassScalars = 0;    // number of mass scalars
 	static constexpr int numPassiveScalars = 3; // number of passive scalars
 	static constexpr int nGroups = 1;	    // number of radiation groups
+	static constexpr UnitSystem unit_system = UnitSystem::CGS;
 };
 
 template <> struct SimulationData<NewProblem> {
