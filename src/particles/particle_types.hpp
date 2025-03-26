606d76094afcb5edcb8b58f036d25399bc6de798
ChongChong He <chongchong.he@anu.edu.au>
2025-03-26 16:38:00 +1100

    add StochasticStellarPopParticleDeathTimeIdx

--- src/particles/particle_types.hpp
+++ src/particles/particle_types.hpp
@@ -171,7 +171,8 @@ enum StochasticStellarPopParticleDataIdx {
 	StochasticStellarPopParticleVxIdx,	  // Velocity in x direction
 	StochasticStellarPopParticleVyIdx,	  // Velocity in y direction
 	StochasticStellarPopParticleVzIdx,	  // Velocity in z direction
-	StochasticStellarPopParticleBirthTimeIdx, // Time when particle becomes active
+	StochasticStellarPopParticleBirthTimeIdx, // Time when a particle is created
+	StochasticStellarPopParticleDeathTimeIdx, // Time when a particle is destroyed/explodes as a SN
 	StochasticStellarPopParticleLumIdx	  // Base index for luminosity components
 };
 
@@ -181,9 +182,9 @@ constexpr int StochasticStellarPopParticleStageIdx = 0; // Evolution stage of th
 template <typename problem_t>
 constexpr int StochasticStellarPopParticleRealComps = []() constexpr {
 	if constexpr (Physics_Traits<problem_t>::is_hydro_enabled && Physics_Traits<problem_t>::is_radiation_enabled) {
-		return 5 + Physics_Traits<problem_t>::nGroups; // mass, vx, vy, vz, birth_time, lum[nGroups]
+		return 6 + Physics_Traits<problem_t>::nGroups; // mass, vx, vy, vz, birth_time, lum[nGroups]
 	} else {
-		return 5; // mass, vx, vy, vz, birth_time
+		return 6; // mass, vx, vy, vz, birth_time
 	}
 }();
 
